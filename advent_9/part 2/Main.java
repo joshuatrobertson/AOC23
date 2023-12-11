25b92916e77ac3aec5d2000d034afb0477ce5047
jrobertson21 <josh.robertson@ibm.com>
2023-12-11 11:22:59 +0000

    day 9 part 2

---
+++ advent_9/part 2/Main.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+    static String input = "/Users/josh/IdeaProjects/advent_9/src/com/company/input.txt";
+    static int total = 0;
+
+    public static void main(String[] args) {
+        try {
+            String lineText;
+            BufferedReader lineReader = new BufferedReader(new FileReader(input));
+
+            while ((lineText = lineReader.readLine()) != null) {
+                ArrayList<Integer> firstNumberList = new ArrayList<Integer>();
+                findFirstNumberSequence(turnStringIntoIntArray(lineText), firstNumberList);
+                int num = 0;
+                for (int i = firstNumberList.size() - 1; i >= 0; i--) {
+                    int currentNumber = firstNumberList.get(i);
+                    num = currentNumber - num;
+                }
+                total+=num;
+            }
+
+            System.out.println("Total = " + total);
+
+            lineReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    private static void findFirstNumberSequence(int[] input, ArrayList<Integer> list) {
+        int[] newNumbers = new int[input.length - 1];
+        list.add(input[0]);
+        for (int i = 0; i < newNumbers.length; i++) {
+            newNumbers[i] = findDifference(input[i], input[i + 1]);
+        }
+        input = newNumbers;
+
+        if (!isArrayFullOfZeros(input)) {
+            findFirstNumberSequence(newNumbers, list);
+        }
+    }
+
+    private static boolean isArrayFullOfZeros(int[] input) {
+        for (int i : input) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int[] turnStringIntoIntArray(String input) {
+        String[] numberStrings = input.split("\\s+");
+
+        int[] intArray = new int[numberStrings.length];
+        for (int i = 0; i < numberStrings.length; i++) {
+            intArray[i] = Integer.parseInt(numberStrings[i]);
+        }
+        return intArray;
+    }
+
+    private static int findDifference(int firstNumber, int secondNumber) {
+        return secondNumber - firstNumber;
+    }
+}
